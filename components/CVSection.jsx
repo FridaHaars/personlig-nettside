dcd0612b656c0053fdcad960ab30c34b4f5d2107
Frida <fha040@uib.no>
2024-04-23 15:20:24 +0200

    styling

--- components/CVSection.jsx
+++ components/CVSection.jsx
@@ -8,9 +8,9 @@
 export function CVSection({ title, sectionTitle, period, description }) {
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-      <h2 className="col-start-1 col-end-2 text-5xl text-secondary-700">
+      <h3 className="col-start-1 col-end-2 text-5xl text-secondary-700">
         {title}
-      </h2>
+      </h3>
       <div className="flex flex-col col-span-2 gap-2">
         <div className="flex flex-col">
           <span className="font-bold">{sectionTitle}</span>
