52dfc14d39540808b6ff36b0eb90dd464c97dcb9
Frida <fha040@uib.no>
2024-04-23 15:16:48 +0200

    attempt 2

--- app/cv/page.jsx
+++ app/cv/page.jsx
@@ -14,7 +14,8 @@ const sections = [
     period: '2018 - pågående',
     description:
       'Pleieassistent hos én bruker -- døgnkontinuerlig skiftarbeid og turnusarbeid.',
-
+  },
+  {
     sectionTitle: 'HP-Sønn Mat AS | Butikkmedarbeider',
     period: '2016 - 2019',
     description:
@@ -26,17 +27,19 @@ const sections = [
     period: 'Aug 2021 - Jun 2024',
     description:
       'Et studie hvor jeg har lært grunnleggende datasikkerhet og programmering. Nøkkelferdigheter innebærer Python/Java, kryptografi, database, OOP, m.m.',
-
+  },
+  {
     sectionTitle: 'Universitetet i Bergen | Årsstudium: Kunsthistorie',
     period: 'Aug 2018 - Jun 2019',
     description:
       'Et studie av det grunnleggende innen kunsthistorie fra den gresk/romerske antikken til i dag, deriblant arkitektur, billedkunst, og analyse av visuelle uttrykk.',
-
+  },
+  {
     sectionTitle: 'Kunstskolen i Bergen | 2-åring fagskoleutdanning innen visuelle fag',
     period: 'Aug 2015 - Mai 2024',
     description:
       'Utøvende kunst over to år, inkludert kurs innen fargelære, videokunst, performance, modellering, visuelle uttrykk, billedkunst, kunsthistorie, m.m.',
-    },
+  },
 ];
 
 export default function CV() {
