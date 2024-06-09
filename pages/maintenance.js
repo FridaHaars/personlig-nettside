49f800dbd1e80f663282571a1ca765c2cc335cef
Frida <fha040@uib.no>
2024-06-10 00:28:12 +0200

    error fix

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -2,7 +2,7 @@ export default function Maintenance() {
     return (
       <div>
         <h1>Maintenance Mode &#128012;</h1>
-        <p style="font-family:'Courier New'">My website is currently undergoing some maintenance. Please check back later!</p>
+        <p>My website is currently undergoing some maintenance. Please check back later!</p>
       </div>
     );
   }
