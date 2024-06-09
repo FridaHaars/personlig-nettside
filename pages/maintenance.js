ab876083f7cd4b7d83eab8b6173167170055d0e1
Frida <fha040@uib.no>
2024-06-10 00:22:43 +0200

    style change

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -1,8 +1,9 @@
 export default function Maintenance() {
     return (
       <div>
-        <h1>Maintenance Mode</h1>
-        <p>My website is currently undergoing maintenance. Please check back later!</p>
+        <h1 style="font-family:'Courier New'">Maintenance Mode</h1>
+        <p style="font-family:'Courier New'">My website is currently undergoing maintenance. Please check back later!</p>
+        <p style="font-size:100px">&#128012;</p>
       </div>
     );
   }
