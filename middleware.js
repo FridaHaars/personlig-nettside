2ce55b5c9ef501fcd1440e8528ea0a76f48c33bb
Frida <fha040@uib.no>
2024-06-10 00:17:15 +0200

    middleware update

--- middleware.js
+++ middleware.js
@@ -3,7 +3,8 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const maintenanceMode = true; // Toggle this value to enable/disable maintenance mode
 
-  if (maintenanceMode) {
+  // Prevent redirect loop
+  if (maintenanceMode && !request.nextUrl.pathname.startsWith('/maintenance')) {
     return NextResponse.redirect(new URL('/maintenance', request.url));
   }
 
