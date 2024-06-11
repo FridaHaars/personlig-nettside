61872e8e4ed17131307bb6db8b9721ef09d504c6
Frida <fha040@uib.no>
2024-06-11 12:49:07 +0200

    maintenance mode

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -1,11 +1,9 @@
 
-export default function Maintenance() {
-    return (
-      <div className="flex items-center justify-center h-screen bg-gray-100">
-        <div className="text-center font-roboto">
-          <h1 className="text-3xl mb-4">Maintenance Mode &#128012;</h1>
-          <p className="text-xl">My website is currently undergoing some maintenance. Please check back later!</p>
-        </div>
-      </div>
-    );
-  }
+export default function MaintenanceMode() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl mb-4">Maintenance Mode &#128012;</h1>
+      <p className="text-xl">My website is currently undergoing some maintenance. Please check back later!</p>
+    </div>
+  );
+}
