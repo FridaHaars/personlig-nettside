7d062aaa755c0f5e343720b5d8594911b77f34d7
Frida <fha040@uib.no>
2024-06-11 12:12:10 +0200

    right adjust

--- app/prosjekter/uib/page.jsx
+++ app/prosjekter/uib/page.jsx
@@ -28,12 +28,15 @@ const images = [
 
 export default async function Project() {
   return (
-    <div className='ml-8 sm:ml-10 flex flex-col gap-16 mb-32 text-right'>
-      <ProjectHero
-        title='Universitetet i Bergen'
-        description='Et utvalg av prosjekter og programmeringsoppgaver fra UiB.'
-        image={backdrop}
-      />
+    <div className='relative w-full h-full'>
+  <div className='absolute right-0 ml-8 sm:ml-10 flex flex-col gap-16 text-right mb-32'>
+    <ProjectHero
+      title='Universitetet i Bergen'
+      description='Et utvalg av prosjekter og programmeringsoppgaver fra UiB.'
+      image={backdrop}
+    />
+    </div>
+
       <Photostrip images={images} />
 
       <TextBlock
